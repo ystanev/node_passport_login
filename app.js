5b656c74bfdca4a0d12894de1989eee950fbb765
Yury Stanev <ystanev@yahoo.ca>
2019-02-04 13:09:16 -0500

    protected route - dashboard

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+//Passport config
+require('./config/passport')(passport);
 
 //DB Config
 const db = require('./config/keys').MongoURI;
@@ -28,6 +32,10 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Connect Flash
 app.use(flash());
 
@@ -35,6 +43,7 @@ app.use(flash());
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 //Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -70,6 +71,22 @@ router.post('/register', (req, res) => {
             }))
     }
 
+});
+
+//Login Handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard', 
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+//Logout Handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logout');
+    res.redirect('/users/login');
 });
 
 module.exports = router;
