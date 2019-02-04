a0cbc194cfdfe639499232347a148494c89a6364
Yury Stanev <ystanev@yahoo.ca>
2019-02-04 11:26:10 -0500

    messages based of results

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 //Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -9,21 +10,32 @@ router.get('/register', (req, res) => res.render('register'));
 
 //Register Handle
 router.post('/register', (req, res) => {
-    const { name, email, password, password2 } = req.body;
-    let errors= [];
+    const {
+        name,
+        email,
+        password,
+        password2
+    } = req.body;
+    let errors = [];
 
     //Check required fields
-    if(!name || !emaiol || !password || !password2){
-        errors.push({ msg: 'Please fill in all fields.' });
+    if (!name || !emaiol || !password || !password2) {
+        errors.push({
+            msg: 'Please fill in all fields.'
+        });
     }
 
     //Check passwords match 
     if (password !== password2) {
-        errors.push({ msg: 'Passwords do not match.' });
+        errors.push({
+            msg: 'Passwords do not match.'
+        });
     }
 
     if (password.length < 6) {
-        errors.push({ msg: 'Password should be at least 6 characters.'});
+        errors.push({
+            msg: 'Password should be at least 6 characters.'
+        });
     }
 
     if (errors.length > 0) {
@@ -34,8 +46,26 @@ router.post('/register', (req, res) => {
             password,
             password2
         });
-    } else{
-        res.send('pass'); 
+    } else {
+        const newUser = new newUser({
+            name,
+            email,
+            password
+        });
+
+        //Hash Password
+        bcrypt.genSalt(10, (err, salt) =>
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                //Set password to hashed
+                newUser.password = hash;
+                //Save the user
+                newUser.save()
+                    .then(user => {
+                        res.redirect('/users/login');
+                    })
+                    .catch(err => console.log(err));
+            }))
     }
 
 });
